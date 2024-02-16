91f04514b76d98d3082c49de752b78eee18d364b
Facu Aragon <facundo_aragon@hotmail.com>
2024-02-16 17:31:21 -0300

    height modifications, and more centered

--- app/page.js
+++ app/page.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col text-center h-screen w-screen">
-        <div className=" mt-24 pb-10">
+        <div className=" mt-44 pb-10">
           <h1 className=" text-6xl pb-2 pt-4">Felix Ramallo</h1>
-          <h3 className=" text-2xl">
+          <h3 className=" text-2xl mx-3">
             Producto, innovación, medios y cultura digital
           </h3>
         </div>
-        <h3 className=" text-2xl pb-2">Suscribite al Newsletter</h3>
+        <h3 className=" text-xl pb-2">Suscribite al newsletter</h3>
         <CustomSubstackWidget />
       </div>
     </>
--- components/CustomSubstrackWidget.js
+++ components/CustomSubstrackWidget.js
@@ -12,7 +12,7 @@ const CustomSubstackWidget = ({
     window.CustomSubstackWidget = {
       substackUrl: substackUrl || "felixramallo.substack.com",
       placeholder: placeholder || "Escribe tu correo electrónico",
-      buttonText: buttonText || "Subscribite",
+      buttonText: buttonText || "Subscribir",
       theme: theme || "custom",
       colors: colors || {
         primary: "#e10613",
@@ -33,7 +33,7 @@ const CustomSubstackWidget = ({
     };
   }, [substackUrl, placeholder, buttonText, theme, colors]); // Dependencias para re-ejecutar si cambian
 
-  return <div id="custom-substack-embed"></div>;
+  return <div id="custom-substack-embed" className=" scale-90 "></div>;
 };
 
 export default CustomSubstackWidget;
